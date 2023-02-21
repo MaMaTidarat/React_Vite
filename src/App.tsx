1dd992571206efadd5616df253fc732eb15e03c1
MaMaTidarat <Tidarat.S@tidlor.com>
2023-02-21 14:53:21 +0700

    style: :art: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import image from './assets/react.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
+export default function App() {
+   const data = import.meta.env.VITE_SOME_KEY;
+   return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <h1 className="text-3xl font-bold underline bg-yellow-200 text-red-300">Hello world!</h1>
+         <img src={image} alt=""></img>
+         <div>{data}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+   );
 }
-
-export default App
